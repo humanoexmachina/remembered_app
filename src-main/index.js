469bb6f3612d0c4d273cdd5326617fb070991cdd
ChunYu Shi <shicyu@icloud.com>
2022-12-27 12:45:43 -0500

    uncomment

--- src-main/index.js
+++ src-main/index.js
@@ -9,7 +9,7 @@ import {
 } from '../backendScripts/service/fileProcessor';
 // import { importSingleChat } from '../backendScripts/service/jsonImporter';
 import { ChatPlatform } from '../backendScripts/util/constants';
-// import { connectRememberedDB } from '../backendScripts/service/db';
+import { connectRememberedDB } from '../backendScripts/service/db';
 
 const appDataDir = `userEnv/appData`; // there is a wiki for setting this with electron
 let userImportedFilePath = '';
